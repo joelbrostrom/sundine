d967680a4b0ee2a7adff16f0cc427e9676229976
Joel Brostrom <joel.brostrom@appshack.se>
2018-04-23 18:18:59 +0200

    Fetch location using google fused Api
    Using googles api for fetching location data
    is a bit more bothersome but offers a lot more
    then androids own location handler.
    In this case I need it in order to use their "Places"
    api which will allow me to fetch nearby businesses.

--- app/src/main/java/com/appshack/sundine/MainActivity.kt
+++ app/src/main/java/com/appshack/sundine/MainActivity.kt
@@ -1,92 +1,165 @@
 package com.appshack.sundine
 
 import android.Manifest
-import android.content.Context
 import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.support.v4.content.PermissionChecker
 import android.util.Log
-import android.widget.Toast
+import com.appshack.sundine.enums.PermissionCodes
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.places.Places
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapFragment
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.karumi.dexter.DexterBuilder
-import java.security.Permission
 
-class MainActivity : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+class MainActivity : FragmentActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, OnMapReadyCallback {
 
-    lateinit var locationClient: FusedLocationProviderClient
+    private val TAG = "@Dev ${javaClass.simpleName}"
+
+    /**
+     *  gMap: The visible map you see
+     *  isResumed: Used to set location only if its the first location since resumed activity
+     *  fusedLocationProviderClient: Googles version of locationClient. Used to fetch location data
+     *  googleApiClient: Handles api connections, most importantly connect and disconnect
+     *  LocationRequest: Callback function passed when fetching data with fusedLocationProviderClient
+     */
+    private lateinit var gMap: GoogleMap
+    private var isResumed: Boolean = false
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    private lateinit var mLocationRequest: LocationRequest
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        locationClient = FusedLocationProviderClient(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = (object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                super.onLocationResult(p0)
+                Log.i(TAG, "LocationCallbackResult: $p0")
+            }
+        })
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000)  // 1 second, in milliseconds
+
+        /**
+         * Call getMapAsync to get a google map.
+         * This call will return a GoogleMap in onMapReady, which we implement from OnMapReadyCallback
+         */
         val mapFragment = fragmentManager.findFragmentById(R.id.google_map_fragment) as MapFragment
         mapFragment.getMapAsync(this)
 
-        val mGoeDataClient = Places.getGeoDataClient(this)
-        val mPlaceDetectionClient = Places.getPlaceDetectionClient(this)
-        val mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
     }
 
-    override fun onMapReady(gMap: GoogleMap?) {
+    /**
+     * We check permission to fetch location via our custom permission handler and pass a function
+     * as the callback parameter
+     * If we have/get permission we save the new googleMap and set isMyLocationEnabled to true,
+     * enabling tracking
+     */
+    override fun onMapReady(googleMap: GoogleMap?) {
 
         val permissionHandler = PermissionHandler(this, listOf(Manifest.permission.ACCESS_FINE_LOCATION), {
             if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
-                gMap?.isMyLocationEnabled = true
+                googleMap?.let {
+                    gMap = googleMap
+                    gMap.isMyLocationEnabled = true
+                }
             }
-            getLocation()
-            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            gMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                    LatLng(locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER).latitude,
-                            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER).longitude)
-                    , 14f))
         })
 
         permissionHandler.checkPermission()
 
-
-//        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
-//            gMap?.isMyLocationEnabled = true
-//        }
-
-
     }
 
-    private fun getLocation() {
+    /**
+     * Set isResumed to true
+     * Activates "go to user location" only the first time the app is resumed
+     * Connect the api client to te google services
+     */
+    override fun onResume() {
+        super.onResume()
+        isResumed = true
+//      setupMapIfNeeded()
+        mGoogleApiClient.connect()
+    }
 
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    /**
+     * If connected when the app pauses (or are terminated) we disconnect and stop listening for
+     * location updates
+     */
+    override fun onPause() {
+        super.onPause()
 
-        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
-
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 20000L, 0f, locationListener)
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20000L, 0f, locationListener)
-
-
-            val lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-            lastLocation?.let { locationListener.onLocationChanged(it) }
-                    ?: locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, locationListener, null)
-
-        } else {
-            Toast.makeText(this, "Could not get location", Toast.LENGTH_LONG).show()
+        if (mGoogleApiClient.isConnected) {
+            fusedLocationClient.removeLocationUpdates(locationCallback)
+            mGoogleApiClient.disconnect()
         }
     }
 
+    override fun onLocationChanged(p0: Location?) {
+        handleNewLocation(p0)
+    }
+
+    /**
+     * If location exists and it's the first location since last resume we get the location and
+     * animates the camera to goto that location and zoom in to zoom level 14
+     */
+    private fun handleNewLocation(location: Location?) {
+
+        location?.let {
+            if (isResumed) {
+                val latLng = LatLng(it.latitude, it.longitude)
+                val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14f)
+                gMap.animateCamera(cameraUpdate)
+                isResumed = false
+            }
+        }
+        Log.d(TAG, location.toString())
+    }
+
+    /**
+     * Create a JoelPermissionHandler (TM) and pass a callback that set a request for location
+     * updates and in turn a callback overriding onLocationResult which pass the current location
+     * along to handleNewLocation
+     */
     override fun onConnected(p0: Bundle?) {
-        if (PermissionChecker.checkSelfPermission(this, ))
-        var lastLocation = locationClient.lastLocation
+        PermissionHandler(this, listOf(Manifest.permission.ACCESS_FINE_LOCATION), {
+
+            if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
+
+                fusedLocationClient.requestLocationUpdates(mLocationRequest, (object : LocationCallback() {
+
+                    override fun onLocationResult(p0: LocationResult?) {
+                        super.onLocationResult(p0)
+                        p0?.let {
+                            handleNewLocation(it.lastLocation)
+                        }
+                    }
+
+                }), null)
+
+
+            }
+        }).checkPermission()
+
     }
 
     override fun onConnectionSuspended(p0: Int) {
@@ -94,46 +167,12 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.Con
     }
 
     override fun onConnectionFailed(p0: ConnectionResult) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onLocationChanged(location: Location?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onProviderEnabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onProviderDisabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-    private val locationListener = object : LocationListener {
-
-        override fun onLocationChanged(location: Location?) {
-            location?.let {
-                Toast.makeText(this@MainActivity, "Lat: ${it.latitude}, Lng: ${it.longitude}", Toast.LENGTH_LONG).show()
-//                posViewModel.loadPos(LatLng(it.latitude, it.longitude))
-
-            } ?: Log.d("@Dev onLocationError", "Location == null")
-        }
-
-        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        }
-
-        override fun onProviderEnabled(provider: String?) {
-        }
-
-        override fun onProviderDisabled(provider: String?) {
+        if (p0.hasResolution()) {
+            p0.startResolutionForResult(this, PermissionCodes.CONNECTION_FAILURE_RESOLUTION_REQUEST.id)
+        } else {
+            Log.i(TAG, "Location services connection failed with code ${p0.errorMessage}")
         }
 
     }
 
 }
-
--- app/src/main/java/com/appshack/sundine/enums/PermissionCodes.kt
+++ app/src/main/java/com/appshack/sundine/enums/PermissionCodes.kt
@@ -7,5 +7,6 @@ package com.appshack.sundine.enums
  */
 enum class PermissionCodes(val id: Int) {
     LOCATION_PERMISSION(1001),
+    CONNECTION_FAILURE_RESOLUTION_REQUEST(9901),
     OPEN_SETTINGS( 999)
 }
