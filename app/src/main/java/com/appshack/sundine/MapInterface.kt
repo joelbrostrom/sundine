23eb50aeed95010b4aa34d2b629b0415fe8dd70a
Joel Brostrom <joel.brostrom@appshack.se>
2018-05-03 15:09:54 +0200

    Create interface for Maps
    To hide unused callback functions and cleaning
    up unused code.

--- app/src/main/java/com/appshack/sundine/MapInterface.kt
+++ app/src/main/java/com/appshack/sundine/MapInterface.kt
@@ -1,9 +1,21 @@
 package com.appshack.sundine
 
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.maps.OnMapReadyCallback
 
 /**
  * Created by joelbrostrom on 2018-05-03
  * Developed by App Shack
  */
-interface MapInterface {
+
+interface MapInterface: GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        OnMapReadyCallback {
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("not implemented") //Not needed for current functionality
+    }
+
 }
